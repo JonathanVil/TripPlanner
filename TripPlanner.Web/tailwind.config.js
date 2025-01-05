fa9cfa4390cc2853281bd4973e0d6719d863f270
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2025-01-05 23:15:54 +0100

    Add flowbite

--- TripPlanner.Web/tailwind.config.js
+++ TripPlanner.Web/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // other files...
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
