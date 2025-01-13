cdfaee7b35f5ab6c18ba357066a4b563ba9fb7d5
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2025-01-14 00:57:03 +0100

    Add Created property to User

---
+++ TripPlanner.Infrastructure/Migrations/20250113235739_AddUserCreatedProp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TripPlanner.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserCreatedProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Users",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Users");
+        }
+    }
+}
