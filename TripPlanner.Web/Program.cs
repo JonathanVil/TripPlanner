0870f0ef1457de19dea97064d453e9a1026bfcb0
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2025-01-15 00:23:03 +0100

    Update database initialization logic in Program.cs
    Moved database initialization to production environment and added a parameter to control migration usage in development. This ensures more specific behavior for each environment and improves setup flexibility.

--- TripPlanner.Web/Program.cs
+++ TripPlanner.Web/Program.cs
@@ -20,13 +20,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    await app.InitializeDatabaseAsync();
+    await app.InitializeDatabaseAsync(useMigrations: false);
 }
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    await app.InitializeDatabaseAsync();
 }
 
 app.UseHttpsRedirection();
