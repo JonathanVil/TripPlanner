b2494c03be42863eab95f9693beba2073ea858c5
Jonathan Antoine Villeret <jvilleret@proton.me>
2025-09-23 23:06:21 +0200

    Set pgAdmin to fixed port

--- TripPlanner.AppHost/Program.cs
+++ TripPlanner.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("tripplanner-postgres")
-    .WithPgAdmin();
+    .WithPgAdmin(configureContainer: resourceBuilder => resourceBuilder.WithHostPort(18123));
 var database = postgres.AddDatabase("tripplanner-db");
 
 builder.AddProject<Projects.TripPlanner_Web>("tripplanner-web")
