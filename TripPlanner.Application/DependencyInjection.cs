4bf56477b2f330538e0d3abeea55c399928db836
Jonathan Antoine Villeret <jvilleret@proton.me>
2025-09-24 18:59:02 +0200

    Add all mappers from assembly to DI

--- TripPlanner.Application/DependencyInjection.cs
+++ TripPlanner.Application/DependencyInjection.cs
@@ -3,6 +3,7 @@ using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using TripPlanner.Application.Common.Behaviours;
+using TripPlanner.Application.Common.Interfaces;
 
 namespace TripPlanner.Application;
 
@@ -10,8 +11,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
+        services.AddMappersFromAssembly(Assembly.GetExecutingAssembly());
+        
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         services.AddMediatR(cfg => {
@@ -22,5 +23,22 @@ public static class DependencyInjection
         });
 
         return services;
+    }
+    
+    private static void AddMappersFromAssembly(this IServiceCollection services, Assembly assembly)
+    {
+        var mapperTypes = assembly.GetTypes()
+            .Where(type => type.IsClass && !type.IsAbstract)
+            .Where(type => type.GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapper<,>)))
+            .ToList();
+
+        foreach (var mapperType in mapperTypes)
+        {
+            var mapperInterface = mapperType.GetInterfaces()
+                .First(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapper<,>));
+
+            services.AddScoped(mapperInterface, mapperType);
+        }
     }
 }
