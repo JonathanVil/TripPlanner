65a1ec8dda5fc08cbc166aadafcc94fe5e2edd02
Jonathan Antoine Villeret <jvilleret@proton.me>
2025-09-24 18:58:48 +0200

    Add shortcut to map entire collections

--- TripPlanner.Application/Common/Interfaces/IMapper.cs
+++ TripPlanner.Application/Common/Interfaces/IMapper.cs
@@ -2,5 +2,15 @@ namespace TripPlanner.Application.Common.Interfaces;
 
 public interface IMapper<in TFrom, out TTo>
 {
+    public IEnumerable<TTo> Map(IEnumerable<TFrom> from)
+    {
+        return from.Select(Map);
+    }
+
+    public TTo[] Map(TFrom[] from)
+    {
+        return from.Select(Map).ToArray();
+    }
+    
     TTo Map(TFrom from);
 }
